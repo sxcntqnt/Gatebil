9f62a3caef27254cabf21d6754b2f60dd9368779
ScienceRocket <sciencerocket@bling.net>
2026-05-25 11:54:18 +0000

    Handling Broken imports

--- backend/routes/ekyc.py
+++ backend/routes/ekyc.py
@@ -20,7 +20,7 @@ from fastapi import APIRouter, Depends, File, UploadFile
 from api.dependency import get_id_detector
 from model.id_detector import IDDetector
 from model.schemas import SmartCropResponse
-from pipeline.ekyc import process_id_card
+from services.ekyc import process_id_card
 
 log = logging.getLogger(__name__)
 
